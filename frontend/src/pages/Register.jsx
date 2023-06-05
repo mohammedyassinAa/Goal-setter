0f84af4fa3140d4433ed3b7ab7b8fb64322661f5
Mohammed Yassin <yssnmed@gmail.com>
2023-06-05 19:15:08 +0100

    new errors

--- frontend/src/pages/Register.jsx
+++ frontend/src/pages/Register.jsx
@@ -3,7 +3,7 @@ import {useSelector , useDispatch } from "react-redux";
 import {useNavigate } from "react-router-dom"
 import {toast} from "react-toastify";
 import Spinner from "../components/Spinner"
-import {register , reset } from "../features/auth/authSlice"
+import {register , reset } from "../features/auth/auth/authSlice"
 import { FaUser } from "react-icons/fa";
 
 
